105ee03159effd234e5c80862cf5351277d0154b
ammaarkhan <ammaarkhan03@gmail.com>
2023-05-06 19:54:44 +0300

    work on app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,10 @@ import { BrowserRouter } from "react-router-dom";
 import { About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, StarCanvas} from './components';
 
 const App = () => {
-
   return (
-    <BrowserRouter>
-    <div className="relative z-0 bg-primary">
-      <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
-        <Navbar/>
-        <Hero />
-
-      </div>
+    <div>
+      3d developer portfolio
     </div>
-    </BrowserRouter>
   )
 }
 
