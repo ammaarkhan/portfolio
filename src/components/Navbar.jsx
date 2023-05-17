6b1ddea09ba2a319ea8a1ed94862503e7487d246
ammaarkhan <ammaarkhan03@gmail.com>
2023-05-16 22:20:30 -0700

    progress on homepage

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -23,6 +23,7 @@ const Navbar = () => {
         }}
         >
           <img src={logo} alt="logo" className='w-9 h-9 object-contain' />
+          <p className='text-white text-[18px] font-bold cursor-pointer'>Ammaar <span className='sm: block hidden'>Khan</span></p>
         </Link>
       </div>
 
