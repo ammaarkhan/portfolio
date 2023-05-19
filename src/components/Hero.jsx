6a42084ba75b9190563acf2a85630eb95f40ee7d
ammaarkhan <ammaarkhan03@gmail.com>
2023-05-19 11:44:16 -0700

    add computer model and debug model not showing

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -10,14 +10,18 @@ const Hero = () => {
       <div className={`${styles.paddingX} absolute inset-0 top-[120px] max-w-7xl mx-auto flex flex-row items-start gap-5`}>
         <div className='flex flex-col justify-center items-center mt-5'>
           <div className='w-5 h-5 rounded-full bg-[#915eff]'/>
-          <div className='w-1 sm:h-80 h-40 violet-gradient'/>
+          <div className='w-1 sm:h-80 h-60 violet-gradient'/>
         </div>
 
         <div>
           <h1 className={`${styles.heroHeadText}text-white`}>Hi, I'm <span className='text-[#915eff]'>Ammaar</span></h1>
-          <p className=''></p>
+          <p className={`${styles.heroSubText} mt-2 text-white-100`}>
+          I work on machine learning, robotics, and develop mobile applications. 
+          </p>
         </div>
       </div>
+
+      <ComputersCanvas />
     </section>
   )
 }
