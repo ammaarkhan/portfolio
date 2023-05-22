4a5f35855ae3e980266e03c0f02d8480109fd8b9
ammaarkhan <ammaarkhan03@gmail.com>
2023-05-21 19:36:38 -0700

    update 3d graphic and create scroll down gif

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -16,12 +16,32 @@ const Hero = () => {
         <div>
           <h1 className={`${styles.heroHeadText}text-white`}>Hi, I'm <span className='text-[#915eff]'>Ammaar</span></h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
-          I work on machine learning, robotics, and develop mobile applications. 
+          CS student passionate about machine learning, robotics, and mobile application development. 
           </p>
         </div>
       </div>
 
       <ComputersCanvas />
+
+      <div className='absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center'>
+        <a href='#about'>
+          <div className='w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2'>
+            <motion.dev
+            animate={{
+              y: [0,24,0]
+            }}
+            transition={{
+              duration: 1.5, 
+              repeat: Infinity,
+              repeatType: 'loop'
+            }}
+            className="w-3 h-3 rounded-full bg-secondary mb-1"
+            />
+          </div>
+
+        </a>
+
+      </div>
     </section>
   )
 }
