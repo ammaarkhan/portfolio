63e831389b005f0a1b037edd5cc4f7f4f2bc2cc9
ammaarkhan <ammaarkhan03@gmail.com>
2023-06-11 22:00:57 -0700

    debug

--- vite.config.js
+++ vite.config.js
@@ -4,13 +4,16 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/Personal-Website/',
-  plugins: [react()],
+  base: process.env.NODE_ENV === 'production' ? '/Personal-Website/' : './',
   build: {
-    outDir: 'dist',
-    emptyOutDir: true,
     rollupOptions: {
       input: '/src/main.jsx',
     },
-  }
+    outDir: 'dist',
+    emptyOutDir: true,
+    assetsDir: '',
+    sourcemap: true,
+    publicPath: process.env.NODE_ENV === 'production' ? '/Personal-Website/' : '/',
+  },
+  plugins: [react()],
 })
