912788d222e48bcd15e9b3ebe968bf0cd1067a1a
ammaarkhan <ammaarkhan03@gmail.com>
2023-06-11 22:23:51 -0700

    edit

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,12 @@
 // vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path'
 
 export default defineConfig({
-  base: './',
+  base: process.env.NODE_ENV === 'production' ? '/Personal-Website/' : './',
   plugins: [react()],
   build: {
     outDir: 'dist',
     emptyOutDir: true,
-    rollupOptions: {
-      input: path.resolve(__dirname, './src/main.jsx'), // Absolute path to entry point
-    },
   },
 })
